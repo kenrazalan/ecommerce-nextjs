c118ad1144b7f162322503dce9be64edce07b3c7
kenrazalan <kennethrazalan29@gmail.com>
2021-07-03 19:44:54 +0800

    add Loader

---
+++ components/Loading.js
@@ -0,0 +1,14 @@
+
+
+const Loading = () =>(
+    <div className="fixed w-full h-full text-center loading"
+    style={{background:"#0008",color:"white", top:0, left:0, zIndex:9}}>
+        <svg width="105" height="250" viewBox="0 0 40 50">
+            <polygon strokeWidth="1" stroke="#fff" fill="none"
+            points="20,1 40,40 1,40"></polygon>
+            <text fill="#fff" x="5" y="47"></text>
+        </svg>
+    </div>
+)
+
+export default Loading
