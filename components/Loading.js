de1d41360a64830b86421cd38a2275e2feef6460
kenrazalan <kennethrazalan29@gmail.com>
2021-07-03 22:19:21 +0800

    zindex loading

--- components/Loading.js
+++ components/Loading.js
@@ -2,7 +2,7 @@
 
 const Loading = () =>(
     <div className="fixed w-full h-full text-center loading"
-    style={{background:"#0008",color:"white", top:0, left:0, zIndex:9}}>
+    style={{background:"#0008",color:"white", top:0, left:0, zIndex:999}}>
         <svg width="105" height="250" viewBox="0 0 40 50">
             <polygon strokeWidth="1" stroke="#fff" fill="none"
             points="20,1 40,40 1,40"></polygon>
