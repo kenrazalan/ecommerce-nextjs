94ecf2ef78db1dc559c23213e0ed6593e5b16bd1
kenrazalan <kennethrazalan29@gmail.com>
2021-07-03 14:52:15 +0800

    add next config

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    reactStrictMode: true,
+    env:{
+      "BASE_URL":"http://localhost:3000",
+      "MONGODB_URL":"mongodb+srv://krazalan29:Kenneth1129@cluster0.y0vx5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      "ACCESS_TOKEN_SECRET": "*-dn%7yjjyk]4>;)9/]qCj^dEAEf:Ld;jh{N)'e_m%By])",
+      "REFRESH_TOKEN_SECRET":"F_,9`Z-d7)68AeeM3#%*qvcP`{+YPM>u`S5fUKuQ<>U,J;w^tG`R<CP<>"
+    }
+  }
+  
