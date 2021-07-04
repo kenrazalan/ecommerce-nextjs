97123b369a0dd9290b587d9c7ce0e1951d25f6ee
kenrazalan <kennethrazalan29@gmail.com>
2021-07-04 21:39:05 +0800

    global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import '../styles/loading.css'
+import '../styles/app.css'
 import Layout from '../components/Layout'
 import { DataProvider } from '../store/GlobalState'
 
