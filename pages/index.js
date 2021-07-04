fc485b61a7ac5274ab1ea2ecd66a89844579cefb
kenrazalan <kennethrazalan29@gmail.com>
2021-07-04 19:44:53 +0800

    Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,34 @@
 import Head from 'next/head'
+import { useState } from 'react'
+import ProductItem from '../components/ProductItem'
+import {getData} from '../utils/fetchData'
 
-export default function Home() {
+export default function Home(props) {
+
+  const [products,setProducts] = useState(props.productProps)
   return (
-    <div className="">
+    <div className="grid grid-cols-3 justify-center">
       <Head>
         <title>Home</title>
       </Head>
-      <h1>home</h1>
+      {
+        products.length === 0 
+        ? <h2>No Products</h2>
+        : products.map(product => (
+          <ProductItem key={product._id} product={product}/>
+        ))
+      }
     </div>
   )
 }
  
+export async function getServerSideProps() {
+  const res = await getData('product')
+  console.log(res)
+  return {
+    props: {
+      productProps: res.products,
+      result: res.result
+    }
+  }
+}
