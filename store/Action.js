79d1d3d6bf040bfacb6356efbc1bb5635d52bf35
kenrazalan <kennethrazalan29@gmail.com>
2021-07-07 00:22:44 +0800

    add delete item

--- store/Action.js
+++ store/Action.js
@@ -34,3 +34,8 @@ export const decrease = (data, id) => {
 
     return ({ type: "ADD_CART",payload: newData})
 }
+
+export const deleteItem = (data, id, type) => {
+    const newData = data.filter( item => item._id !== id )
+    return ({type, payload: newData})
+}
