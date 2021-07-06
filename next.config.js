cd6a1410f4a146511570000a2083ab88b17a6fa3
kenrazalan <kennethrazalan29@gmail.com>
2021-07-07 01:24:21 +0800

    paypal button

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ module.exports = {
       "BASE_URL":"http://localhost:3000",
       "MONGODB_URL":"mongodb+srv://krazalan29:Kenneth1129@cluster0.y0vx5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
       "ACCESS_TOKEN_SECRET": "*-dn%7yjjyk]4>;)9/]qCj^dEAEf:Ld;jh{N)'e_m%By])",
-      "REFRESH_TOKEN_SECRET":"F_,9`Z-d7)68AeeM3#%*qvcP`{+YPM>u`S5fUKuQ<>U,J;w^tG`R<CP<>"
+      "REFRESH_TOKEN_SECRET":"F_,9`Z-d7)68AeeM3#%*qvcP`{+YPM>u`S5fUKuQ<>U,J;w^tG`R<CP<>",
+      "PAYPAL_CLIENT": "AZBF-kF8ifaBwyX02tVPD1yl6OY58twrzT69zfohJk9LkaaISCuxPFlwENGRCk3NSBeQpZE3wH40IxfV"
+
     }
   }
   
---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, {Html, Head, Main, NextScript} from 'next/document'
+
+class MyDocument extends Document{
+    render(){
+        return(
+            <Html lang="en">
+                <Head>
+                    <meta name="description" content="Ecommerce NextJs"/>
+                    <script src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT}`}></script>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
