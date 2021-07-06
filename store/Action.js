9c0218fb153786f54b5240599ec7b00448dd8a68
kenrazalan <kennethrazalan29@gmail.com>
2021-07-06 23:30:30 +0800

    add increase decrese method

--- store/Action.js
+++ store/Action.js
@@ -16,3 +16,21 @@ export const addToCart = (product,cart) => {
 
     return ({ type: "ADD_CART", payload: [...cart, {...product, quantity: 1 } ]})
 }
+
+export const increase = (data, id) => {
+    const newData = [...data]
+    newData.forEach(item => {
+        if(item._id === id ) item.quantity += 1 
+    })
+
+    return ({ type: "ADD_CART",payload: newData})
+}
+
+export const decrease = (data, id) => {
+    const newData = [...data]
+    newData.forEach(item => {
+        if(item._id === id ) item.quantity -= 1 
+    })
+
+    return ({ type: "ADD_CART",payload: newData})
+}
