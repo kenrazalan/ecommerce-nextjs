a552951211368fd2265ce3bb21f322d5ebe895aa
kenrazalan <kennethrazalan29@gmail.com>
2021-07-07 22:29:15 +0800

    update stock and sold in database

--- pages/api/order/index.js
+++ pages/api/order/index.js
@@ -16,8 +16,28 @@ export default async (req,res) => {
 const createOrder = async (req,res) => {
     try{
         const result = await auth(req,res)
-        console.log(result)
+        const {address, mobile, cart, total} = req.body;
+
+        const newOrder = new Orders({
+            user: result.id, address, mobile, cart, total
+        })
+
+        cart.filter(item => {
+            return sold(item._id, item.quantity, item.inStock, item.sold)
+        })
+        await newOrder.save()
+        res.json({
+            msg: 'Payment Success.',
+            newOrder
+        })
     }catch(err){
         return res.status(500).json({ err: err.message})
     }
+}   
+
+const sold = async (id, quantity, oldInStock, oldSold) => {
+    await Products.findOneAndUpdate({_id: id}, {
+        inStock: oldInStock - quantity,
+        sold: oldSold + quantity
+    })
 }
