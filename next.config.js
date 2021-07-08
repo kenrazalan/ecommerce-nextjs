3974a8c4a432ceca4c4ae5e366e4d7d0ad88c716
kenrazalan <kennethrazalan29@gmail.com>
2021-07-08 23:11:23 +0800

    cloudinary env

--- next.config.js
+++ next.config.js
@@ -5,7 +5,10 @@ module.exports = {
       "MONGODB_URL":"mongodb+srv://krazalan29:Kenneth1129@cluster0.y0vx5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
       "ACCESS_TOKEN_SECRET": "*-dn%7yjjyk]4>;)9/]qCj^dEAEf:Ld;jh{N)'e_m%By])",
       "REFRESH_TOKEN_SECRET":"F_,9`Z-d7)68AeeM3#%*qvcP`{+YPM>u`S5fUKuQ<>U,J;w^tG`R<CP<>",
-      "PAYPAL_CLIENT": "AZBF-kF8ifaBwyX02tVPD1yl6OY58twrzT69zfohJk9LkaaISCuxPFlwENGRCk3NSBeQpZE3wH40IxfV"
+      "PAYPAL_CLIENT": "AZBF-kF8ifaBwyX02tVPD1yl6OY58twrzT69zfohJk9LkaaISCuxPFlwENGRCk3NSBeQpZE3wH40IxfV",
+      "CLOUD_UPDATE_PRESET": "next-js",
+      "CLOUD_NAME": "dtwrzv0of",
+      "CLOUD_API": "https://api.cloudinary.com/v1_1/dtwrzv0of/image/upload"
 
     }
   }
