e87906b336d1e26404e652613fccdef1e41a5f8a
kenrazalan <kennethrazalan29@gmail.com>
2021-07-09 22:51:02 +0800

    get orders api

--- pages/api/order/index.js
+++ pages/api/order/index.js
@@ -11,12 +11,33 @@ export default async (req,res) => {
             await createOrder(req,res)
             break;
     }
+    switch(req.method){
+        case "GET":
+            await getOrders(req,res)
+            break;
+    }
 }
 
+const getOrders = async(req,res) => {
+    try {
+        const result = await auth(req,res)
+        let orders;
+        if(result.role !== 'admin'){
+            orders = await Orders.find({user: result.id}).populate("user","-password")
+        }else{
+            orders = await Orders.find().populate("user","-password")
+        }
+        res.json({orders})
+
+    } catch (err) {
+        return res.status(500).json({ err: err.message})
+    }
+}
 const createOrder = async (req,res) => {
     try{
         const result = await auth(req,res)
         const {address, mobile, cart, total} = req.body;
+
 
         const newOrder = new Orders({
             user: result.id, address, mobile, cart, total
