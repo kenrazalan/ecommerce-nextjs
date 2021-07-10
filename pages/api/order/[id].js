687372b7bce4403e1982d175efa735a7af56dbea
kenrazalan <kennethrazalan29@gmail.com>
2021-07-10 22:09:23 +0800

    payment order api

---
+++ pages/api/order/[id].js
@@ -0,0 +1,31 @@
+import connectDB from '../../../utils/connectDB'
+import Orders from '../../../models/orderModel'
+import Products from '../../../models/productModel'
+import auth from '../../../middleware/auth'
+
+connectDB()
+
+export default async (req,res) => {
+    switch(req.method){
+        case "PATCH":
+            await paymentOrder(req,res)
+            break;
+    }
+}
+
+const paymentOrder = async (req,res) => {
+    try {
+        const result = await auth(req,res)
+        const {id} = req.query
+
+        await Orders.findOneAndUpdate({_id: id}, {
+            paid: true, dateOfPayment: new Date().toISOString()
+        })
+        console.log('Success')
+        res.json({msg: 'Payment Success!'})
+        
+    } catch (error) {
+        return res.status(500).json({err: err.message})
+        
+    }
+}
